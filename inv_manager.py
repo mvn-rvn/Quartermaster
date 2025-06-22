fc14eb4cad468abdafa9696c565379cbab1636d1
mvn-rvn <xismiranims@hotmail.com>
2025-06-22 18:00:56 -0400

    d

---
+++ inv_manager.py
@@ -0,0 +1,10 @@
+import discord
+
+bot = discord.Bot()
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+bot.run(open("token.txt").read())
