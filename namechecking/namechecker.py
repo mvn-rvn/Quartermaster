f9d37c1ec309aab6b8cc14ebc271c47253bdf60b
mvn-rvn <maven_raven_was_taken@proton.me>
2025-06-27 19:03:58 -0400

    consolidated namechecker.py into one function
    Function returns:
    -bool (does item already exist in database)
    -str (database item's name with cases)
    -bool (is database item secret)

--- namechecking/namechecker.py
+++ namechecking/namechecker.py
@@ -2,30 +2,8 @@ import discord
 import aiosqlite
 
 
-async def check_exists_caseins(ctx: discord.ApplicationContext, input_name: str) -> tuple[bool, str]:
+async def check_exists_caseins(ctx: discord.ApplicationContext, input_name: str) -> tuple[bool, str, bool]:
     
-    db = await aiosqlite.connect("inv_manager.db")
-    cursor = await db.cursor()
-    
-    await cursor.execute("""
-        SELECT ItemExact
-        FROM MasterInv
-        WHERE ServerID = ? AND ItemLower = ?
-    """, (ctx.guild.id, input_name.lower()))
-
-    db_name: str = await cursor.fetchone()[0]
-    
-    await cursor.close()
-    await db.close()
-    
-    if db_name == None:
-        return (False, input_name)
-    else:
-        return (True, db_name)
-
-
-async def check_secret_name_conflict(ctx: discord.ApplicationContext, input_name: str, input_secret: bool) -> bool:
-
     db = await aiosqlite.connect("inv_manager.db")
     cursor = await db.cursor()
     
@@ -34,19 +12,19 @@ async def check_secret_name_conflict(ctx: discord.ApplicationContext, input_name
         FROM MasterInv
         WHERE ServerID = ? AND ItemLower = ?
     """, (ctx.guild.id, input_name.lower()))
-    
-    data: tuple[str, int] = await cursor.fetchone()
-    
-    if data == None:
-        await cursor.close()
-        await db.close()
-        return False
+
+    data = await cursor.fetchone()
     
     await cursor.close()
     await db.close()
     
+    if data == None:
+        return (False, None, None)
+    
+    db_name: str = data[0]
+    
     db_secret: bool = False
     if data[1] == 1:
         db_secret = True
-
-    return input_secret == db_secret
+    
+    return (True, db_name, db_secret)
