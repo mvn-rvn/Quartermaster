1addd6db6db91047d48118f71631857c7c8f036c
mvn-rvn <maven_raven_was_taken@proton.me>
2025-06-27 19:12:25 -0400

    BasicInvManip boilerplate finished

--- cogs/basicinvmanip.py
+++ cogs/basicinvmanip.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
-from discord.commands import SlashCommandGroup
 import aiosqlite
 
 from permshandling import permshandler
 from commonerrmsgs import errmsgs
+from namechecking import namechecker
 
 #includes give, transfer, remove, inventory, peek
 class BasicInvManip(commands.Cog):
+    
+    
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
